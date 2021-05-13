b140f44580aba77c8aa784906ac9890c53343156
Robert Mainhardt <rmainhar@shakopee.k12.mn.us>
2021-05-13 16:35:40 -0500

    Create script.js

---
+++ Version6.0/user00/JS/script.js
@@ -0,0 +1,12 @@
+function changeText() {
+    document.getElementById("textChange").innerHTML="<u style='background-color:red;'>Thanks for liking my Webpage</u>";
+   }
+function findTotal(){
+    var arr = document.getElementsByName('qty');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+            tot += parseInt(arr[i].value);
+    }
+    document.getElementById('total').value = tot;
+}
